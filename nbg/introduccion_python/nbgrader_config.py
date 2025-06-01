90119c58a2e3786af554ab8a1a335aaf430ca372
Luis M. de la cruz <luiggix@gmail.com>
2025-06-01 19:54:00 +0000

    evaluacion

---
+++ nbg/introduccion_python/nbgrader_config.py
@@ -0,0 +1,9 @@
+c = get_config() 
+
+########################################
+# MACTI: nbgrader_config.py 
+########################################
+c.CourseDirectory.course_id = "introduccion_python" 
+c.IncludeHeaderFooter.header = "source/header.ipynb" 
+c.Exchange.root = "/usr/local/share/nbgrader/exchange" 
+c.CourseDirectory.root = "/home/jovyan/introduccion_python/nbg/introduccion_python" 
