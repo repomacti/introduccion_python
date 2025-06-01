94d79ec3f88929b0f653ab291e6121c32775cbf5
Luis M. de la cruz <luiggix@gmail.com>
2025-06-01 20:28:26 +0000

    release for repomacti

--- nbg/introduccion_python/nbgrader_config.py
+++
@@ -1,9 +0,0 @@
-c = get_config() 
-
-########################################
-# MACTI: nbgrader_config.py 
-########################################
-c.CourseDirectory.course_id = "introduccion_python" 
-c.IncludeHeaderFooter.header = "source/header.ipynb" 
-c.Exchange.root = "/usr/local/share/nbgrader/exchange" 
-c.CourseDirectory.root = "/home/jovyan/introduccion_python/nbg/introduccion_python" 
